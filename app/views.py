a989b6a89093ba9b876b90d1d548324af0f5e674
Edward Karuna <edward.karuna@gree.co.jp>
2016-01-30 17:20:59 -0800

    Add crawl and percentile functionality.

--- app/views.py
+++ app/views.py
@@ -5,10 +5,16 @@ from flask_socketio import emit
 from . import app, socketio
 import json
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Welcome to the Star Wars Dice Roller! Go to /roll to start!'
+
+
+@app.route('/crawl')
+def crawl():
+    return render_template('crawl.html')
 
 BoostResult = ["b0", "b0", "b1s", "b1s1a", "b2a", "b1a"]
 SetbackResult = ["s0", "s0", "s1f", "s1f", "s1t", "s1t"]
@@ -58,6 +64,7 @@ def parse_result(result):
         retval = retval + str(triumphs) + " Triumph "
     return retval
 
+
 @app.route('/roll', methods=['GET', 'POST'])
 def roll():
     form = RollForm()
@@ -85,26 +92,37 @@ def roll():
                 result.append(random.choice(ChallengeResult))
         if form.force.data:
             for i in range(form.force.data):
-                result.append(random.choice(ForceResult));
+                result.append(random.choice(ForceResult))
         data = {'dice': result, 'player': player, 'total': parse_result(result)}
+        if form.percentile.data:
+            for i in range(form.percentile.data):
+                if data['total'] == '':
+                    data['total'] = str(random.randint(0,99))
+                else:
+                    data['total'] = data['total'] + ' and ' + str(random.randint(0,99))
+            data['total'] = data['total'] + ' on percentile'
         socketio.emit('edroll', {'data': json.dumps(data)}, namespace='/swdice')
         return render_template('roll.html', form=RollForm(), result=result)
     return render_template('roll.html', form=form, result=result)
+
 
 @socketio.on('connect', namespace='/swdice')
 def socket_connect():
     print ('Socket Connected!')
 
+
 @socketio.on('disconnect', namespace='/swdice')
 def socket_disconnect():
     print ('Socket Disconnected!')
+
 
 @app.route('/monitor')
 def monitor():
     print ('Loading monitor')
     return render_template('monitor.html')
 
+
 @socketio.on_error_default
 def default_error_handler(e):
-    print(request.event["message"]) # "my error event"
-    print(request.event["args"])    # (data,)
+    print(request.event["message"])  # "my error event"
+    print(request.event["args"])     # (data,)
